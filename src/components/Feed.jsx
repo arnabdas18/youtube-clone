c730e7b3128016e49fa35d78f1c383e7a07afa8e
Arnab Das <86297998+arnabdas18@users.noreply.github.com>
2022-08-31 15:01:16 +0530

    completed project

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -2,8 +2,19 @@ import React, { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import { Sidebar, Videos } from './';
+import { fetchFromApi } from '../utils/fetchFromApi';
 
 const Feed = () => {
+
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${selectedCategory}`)
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
+  
+
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box sx={{ 
@@ -11,7 +22,10 @@ const Feed = () => {
         borderRight: '1px solid #3d3d3d', 
         px: { sx: 0, md: 2 } 
       }}>
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff' }}>
           Copyright 2022 YT2.0
         </Typography>
@@ -23,9 +37,9 @@ const Feed = () => {
           mb={2}
           sx={{ color: 'white'}}
         >
-          New <span style={{color: '#f31503'}}>videos</span>
+          {selectedCategory} <span style={{color: '#f31503'}}>videos</span>
         </Typography>
-        <Videos videos={[]} />
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
